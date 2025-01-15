acc17017b4d298f8c3c1ed2e7cfe052ef136bc0f
avilliai <1840094972@qq.com>
2025-01-15 08:56:28 +0000

    f

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,7 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        定位是sdk而非开发框架。
+        eridanus-dep的定位为sdk，轻便易安装。
       </>
     ),
   },
@@ -17,7 +17,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        尽可能以符合直觉的方式来构建onebot应用，简单易上手。
+        符合直觉，简单易上手。
       </>
     ),
   },
@@ -26,7 +26,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        目前暂未将作为sdk的developTools和作为onebot应用的Eridanus相分离。后续将发布到pypi
+        基于eridanus-dep开发的Eridanus提供了丰富的功能。
       </>
     ),
   },
